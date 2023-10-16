8b83116f8bd038eebc3bb95791cbe18d8918e0f1
el-sam <samurai.h@yahoo.com>
2023-10-16 18:03:27 +0800

    define arrow function square which times itself for the square logic

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ add(2,3);
 // define function divide using function expression
 const divide = function (a, b) { return a/b};
 divide(2000,100);
+
+// define arrow function square
+const square = x => x*x;
